82adb019f6c331601e131e9a9b5405e0903b7279
Dmitrii Valentinov <d.valentinov@gmail.com>
2024-12-05 09:03:08 +0100

    Change text on HomePage

--- src/modules/Homepage.js
+++ src/modules/Homepage.js
@@ -67,23 +67,12 @@ let data = {
 function Homepage () {
   return(
     <div className="body-section">
-      <div className="home-description d-flex flex-row">
-        <img className="homeimg" src={homeimg} alt="service foto"/>
-        <div className="home-text">
-          <p>My name is Yuliya Belikova, I am a makeup artist and hair stylist. I see the beauty in everyone and in my work I try to emphasize the natural beauty of each client. In the profession since 2019. I am constantly following the trends and new products of the beauty industry.</p> 
-          <p>When you come to me, you:</p>
-          <ul>
-            <li>Get a beautiful, modern, long-lasting makeup and a beautiful hairstyle.</li>
-            <li>In individual lessons, you will master the knowledge and skills of actual makeup for yourself. You will be able to make it yourself at home without spending a lot of time.</li>
-            <li>Get high-quality service - I use the best products and cosmetics in my work.</li>
-            <li>Please yourself and be inspired to make changes in your life.</li>
-          </ul>
-          <p>Why am I being chosen: </p>
-          <ul>
-            <li>Professionalism. I am constantly improving my knowledge and introducing the best practices into my practice.</li>
-            <li>Neatness and cleanliness. I carefully process the brushes after each client, using disposable tools where necessary.</li>
-            <li>Comfort. I am able to hear the wishes of clients and their needs.</li>
-          </ul>   
+      <div className="container home-description d-flex flex-row">
+        <img className="homeimg col-sm-12" src={homeimg} alt="service foto"/>
+        <div className="col-sm-12 home-text">
+          <p>If you need a beautiful, modern makeup and hairstyle, you are in the right place!</p> 
+          <p>The principles of my work are to emphasize your strengths, to charge you with self—love and self-confidence throughout the day. To provide a high-quality service for a comfortable time and optimal cost.</p>
+          <p>To inspire you to further transform.</p>
         </div> 
       </div>
       <HomepageServices services={data.services} />
